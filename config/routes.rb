cab4f406bbb4de933f51f28da757c78d8e521d3d
AustinTheTacoMan <austin.jarnagin@dcgschools.com>
2017-12-20 09:32:53 -0600

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
+
+
 Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
 
+  get 'static_pages/h'
+
+  get  'static_pages/home'
+  get  'static_pages/help'
   root 'application#hello'
 end
+
+
