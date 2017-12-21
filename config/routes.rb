9db92d5e71336f6045a0ef0ca2ca574703a0e2d6
AustinTheTacoMan <austin.jarnagin@dcgschools.com>
2017-12-21 09:29:53 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
-
-
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/h'
-
+#   root 'static_pages#home'
   get  'static_pages/home'
   get  'static_pages/help'
-  root 'application#hello'
+  get  'static_pages/about'
 end
-
-
